634fdae9ebe744837a61aff60786c2c065d8d1d8
sophie-senftleben <develop@sophie-senftleben.de>
2018-04-14 09:08:52 +0200

    changed project name and updated gradle version

--- app/src/test/java/com/example/android/businessdetails/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/businessdetails/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package de.sophie_senftleben.businessdetails;
+package com.example.android.businessdetails;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
